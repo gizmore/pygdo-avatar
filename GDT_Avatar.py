ab57d46d360056476977a9e6ef3bf9cf4e45d05d
gizmore <gizmore@wechall.net>
2024-06-15 06:49:56 +0200

    'chappy sync'

--- GDT_Avatar.py
+++ GDT_Avatar.py
@@ -1,9 +1,12 @@
 from gdo.core.GDO_User import GDO_User
-from gdo.core.WithGDO import WithGDO
 from gdo.ui.GDT_Image import GDT_Image
 
 
-class GDT_Avatar(WithGDO, GDT_Image):
+class GDT_Avatar(GDT_Image):
+    _avatar_user: GDO_User
+
+    def __init__(self, name: str):
+        super().__init__(name)
 
     def for_user(self, user: GDO_User):
         self.gdo(user)
