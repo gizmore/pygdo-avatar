0938f8e657bd742430c76276bf19246e4c483f15
Christian Busch <cb@oncrea.de>
2025-01-01 20:03:05 +0100

    'asgi'

--- test/test_avatar.py
+++ test/test_avatar.py
@@ -14,10 +14,10 @@ class AvatarTest(unittest.TestCase):
     def setUp(self):
         Application.init(os.path.dirname(__file__ + "/../../../../"))
         Application.init_cli()
-        Application.set_session(GDO_Session.for_user(web_gizmore()))
         loader = ModuleLoader.instance()
         loader.load_modules_db()
         loader.init_modules(load_vals=True)
+        Application.set_session(GDO_Session.for_user(web_gizmore()))
         install_module('avatar')
         return self
 
@@ -35,7 +35,7 @@ class AvatarTest(unittest.TestCase):
         self.assertIn('avatar', out, 'cannot render avatar.')
 
     def test_03_upload_text(self):
-        web_plug('core.echo.html').exec()
+        web_plug('core.whoami.html').exec()
         data = b'-----------------------------283687824923932629242982017982\r\nContent-Disposition: form-data; name=\"test\"\r\n\r\ngjgjgj\r\n-----------------------------283687824923932629242982017982\r\nContent-Disposition: form-data; name=\"image\"; filename=\"AnschreibenAndrena.txt\"\r\nContent-Type: text/plain\r\n\r\nHallo,\n\nIch bewerbe mich f\\xc3\\xbcr eine Anstellung als Programmierer,\n\nIch heisse Christian, bin 43 Jahre alt, und komme aus Peine, Niedersachsen.\n\nIch spreche einige Programmiersprachen fliessend, darunter PHP, Java, Jacascript, Python, Ruby und C.\n\nIm Design bin ich nicht der beste, kann aber Anforderungen in CSS umsetzen.\n\nIch bin der Kopf und Programmierer hinter www.wechall.net und kenne mich auch in Datenbanken etwas aus.\n\nMeherer GB CSV streame ich zum Fr\\xc3\\xbchst\\xc3\\xbcck ^^\n\nIch hoffe ich habe interesse geweckt.\n\nViele Gr\\xc3\\xbc\\xc3\\x9fe\nChristian Busch\ngizmore@wechall.net\n\n\r\n-----------------------------283687824923932629242982017982\r\nContent-Disposition: form-data; name=\"csrf\"\r\n\r\n243e71619862\r\n-----------------------------283687824923932629242982017982\r\nContent-Disposition: form-data; name=\"submit\"\r\n\r\nSubmit\r\n-----------------------------283687824923932629242982017982--\r\n'
         out = web_plug('avatar.upload.html').post_multipart(data, '---------------------------283687824923932629242982017982').exec()
         self.assertIn('format is not supported', out, 'Can upload text file to avatars.')
